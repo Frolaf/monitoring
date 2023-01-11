8a08db263ffc86aabc4f45a7b3bc905a5b0364e1
Frolaf <78124286+Frolaf@users.noreply.github.com>
2023-01-11 11:00:43 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,61 @@
+status1="";
+objects=[];
+
+function setup(){
+    canvas = createCanvas(480,380);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(380,380);
+    video.hide();
+}
+
+function draw(){
+    image(video,0,0,480,380);
+    if(status1 != ""){
+        objectDetector.detect(video, gotResult);
+        for(i=0; i<objects.length; i++){
+            document.getElementById("status").innerHTML = "status: objects detected";
+            document.getElementById("number_of_objects").innerHTML = "number of objects detected are: " + objects.length;
+            fill("#FF0000");
+            percent = floor(objects[i].confidence*100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+
+        if(objects[i].label == "person") { 
+            document.getElementById("number_of_objects").innerHTML = "Baby Found";
+            console.log("stop"); song.stop();
+         }
+        else {
+             document.getElementById("number_of_objects").innerHTML = "Baby Not Found"; 
+             console.log("play"); 
+             song.play(); 
+            } 
+             if(objects.length == 0) { 
+                 document.getElementById("number_of_objects").innerHTML = "Baby Not Found"; 
+                 console.log("play"); song.play();
+             }
+    }
+}
+
+function gotResult(error, results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        objects = results;
+    }
+}
+
+function start(){
+    objectDetector= ml5.objectDetector("cocossd",modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log(" Model Loaded ");
+    status1 = true;
+}
